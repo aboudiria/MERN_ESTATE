d6cd5ec048c12fcbb5cad0343624a38489c9ad5b
abdel karim dawoud <dawoudria@gmail.com>
2024-04-20 10:56:48 +0300

    create Header and pages after installing Reactjs + tailwindCSS

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import Home from './pages/Home';
+import Header from './components/Header';
+import Signin from './pages/Signin';
+import SignUp from'./pages/SignUp';
+import About from './pages/About';
+import Profile from './pages/Profile';
+
+export default function App() {
+  return (
+   <BrowserRouter>
+   <Header/>
+     <Routes>
+       <Route path='/' element={<Home />}/>
+       <Route path='/sign-in' element={<Signin />} />
+       <Route path='/sign-up' element={<SignUp />} />
+       <Route path='/profile' element={<Profile />} />
+       <Route path='/about' element={<About />} />
+
+     </Routes>
+   </BrowserRouter>
+  )
+}
